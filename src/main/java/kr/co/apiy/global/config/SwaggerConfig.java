f397ccf31c55bf39e5188fa0499911b78e80264e
윤건수 <ygs3004@naver.com>
2024-07-07 03:07:44 +0900

    - Swagger OpenApiCustomizer Example

--- src/main/java/kr/co/apiy/global/config/SwaggerConfig.java
+++ src/main/java/kr/co/apiy/global/config/SwaggerConfig.java
@@ -2,14 +2,41 @@ package kr.co.apiy.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.log4j.Log4j2;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Log4j2
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Components components = new Components();
+        components.addSecuritySchemes(
+                "bearer-key",
+                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(apiInfo())
+                .components(components);
+    }
+
+    public Info apiInfo() {
+        return new Info()
+                .title("ApiY")
+                .version("1.0")
+                .description("Swagger 화면");
+    }
+
 
     @Bean
     public GroupedOpenApi groupMember(){
@@ -26,28 +53,22 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("Sample")
                 .pathsToMatch(pathToMatch)
+                .addOpenApiCustomizer(authOperationCustomizer()) // 태그 한번에 추가하기 가능
                 .build();
     }
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(apiInfo())
-                .components(new Components()
-                        .addSecuritySchemes(
-                                "bearer-key",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+    public OpenApiCustomizer authOperationCustomizer() {
+        return openApi -> {
+            Paths paths = openApi.getPaths();
+            if (paths != null) {
+                paths.forEach((path, pathItem) -> {
+                    pathItem.readOperations().forEach(operation -> {
+                                SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearer-key");
+                                operation.addSecurityItem(securityRequirement);
+                    });
+                });
+            }
+        };
     }
-
-    public Info apiInfo() {
-        return new Info()
-                .title("ApiY")
-                .version("1.0")
-                .description("Swagger 화면");
-    }
-
 
 }
