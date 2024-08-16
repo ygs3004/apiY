dcba557750c72773bf5cad9d6aca1db743f89fbd
ygs <ygs3004@naver.com>
2024-08-17 05:34:56 +0900

    test

--- frontend/src/views/today/today.vue
+++ frontend/src/views/today/today.vue
@@ -4,16 +4,14 @@ import NewsList from "@/views/today/news-list.vue";
 </script>
 
 <template>
+  <h1>테스트</h1>
   <v-col cols="12" md="3">
     <v-row>
+      <h1>테스트</h1>
       <news-list/>
     </v-row>
   </v-col>
 </template>
 
 <style scoped>
-.today-container{
-  width:100vw;
-  height: 100vh;
-}
 </style>
